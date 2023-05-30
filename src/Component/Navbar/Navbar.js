0666cc6e7fa29e7589095b007b8020aab61bf568
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-05-31 00:02:27 +0530

    changes

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
                         {/* <!--=============== DROPDOWN 1 ===============--> */}
                         <li className="dropdown__item">                      
                             <div className="nav__link dropdown__button">
-                                Discover <i className="ri-arrow-down-s-line dropdown__arrow"></i>
+                                About <i className="ri-arrow-down-s-line dropdown__arrow"></i>
                             </div>
 
                             <div className="dropdown__container">
@@ -36,17 +36,17 @@ const Navbar = () => {
                                             <i className="ri-flashlight-line"></i>
                                         </div>
     
-                                        <span className="dropdown__title">Most viewed courses</span>
+                                        <span className="dropdown__title">Codebird Team</span>
     
                                         <ul className="dropdown__list">
                                             <li>
-                                                <a href="#" className="dropdown__link">HTML for beginners</a>
+                                                <a href="#" className="dropdown__link">Current Team</a>
                                             </li>
                                             <li>
-                                                <a href="#" className="dropdown__link">Advanced CSS</a>
+                                                <a href="#" className="dropdown__link">Founding Team</a>
                                             </li>
                                             <li>
-                                                <a href="#" className="dropdown__link">JavaScript OOP</a>
+                                                <a href="#" className="dropdown__link">Website creating team</a>
                                             </li>
                                         </ul>
                                     </div>
@@ -56,17 +56,20 @@ const Navbar = () => {
                                             <i className="ri-heart-3-line"></i>
                                         </div>
     
-                                        <span className="dropdown__title">Popular courses</span>
+                                        <span className="dropdown__title">Popular Domains</span>
     
                                         <ul className="dropdown__list">
                                             <li>
-                                                <a href="#" className="dropdown__link">Development with Flutter</a>
+                                                <a href="#" className="dropdown__link">Web development</a>
                                             </li>
                                             <li>
-                                                <a href="#" className="dropdown__link">Web development with React</a>
+                                                <a href="#" className="dropdown__link">App development</a>
                                             </li>
                                             <li>
-                                                <a href="#" className="dropdown__link">Backend development expert</a>
+                                                <a href="#" className="dropdown__link">CP/DSA</a>
+                                            </li>
+                                            <li>
+                                                <a href="#" className="dropdown__link">ML/AI/DL</a>
                                             </li>
                                         </ul>
                                     </div>
@@ -76,20 +79,21 @@ const Navbar = () => {
                                             <i className="ri-book-mark-line"></i>
                                         </div>
     
-                                        <span className="dropdown__title">Careers</span>
+                                        <span className="dropdown__title">Careers Domain</span>
+                                        
     
                                         <ul className="dropdown__list">
                                             <li>
-                                                <a href="#" className="dropdown__link">Web development</a>
+                                                <a href="#" className="dropdown__link">Cybersecurity</a>
                                             </li>
                                             <li>
-                                                <a href="#" className="dropdown__link">Applications development</a>
+                                                <a href="#" className="dropdown__link">Blockchain/Web3</a>
                                             </li>
                                             <li>
-                                                <a href="#" className="dropdown__link">UI/UX design</a>
+                                                <a href="#" className="dropdown__link">Game development</a>
                                             </li>
                                             <li>
-                                                <a href="#" className="dropdown__link">Informatic security</a>
+                                                <a href="#" className="dropdown__link">Hardware</a>
                                             </li>
                                         </ul>
                                     </div>
@@ -103,11 +107,12 @@ const Navbar = () => {
     
                                         <ul className="dropdown__list">
                                             <li>
-                                                <a href="#" className="dropdown__link">Course certificates</a>
+                                                <a href="#" className="dropdown__link">CodeBird Member certificates</a>
                                             </li>
                                             <li>
-                                                <a href="#" className="dropdown__link">Free certifications</a>
+                                                <a href="#" className="dropdown__link">CodeBird others certificates</a>
                                             </li>
+                                            
                                         </ul>
                                     </div>
                                 </div>
@@ -127,14 +132,14 @@ const Navbar = () => {
                                             <i className="ri-code-line"></i>
                                         </div>
     
-                                        <span className="dropdown__title">Web templates</span>
+                                        <span className="dropdown__title">Adviser</span>
     
                                         <ul className="dropdown__list">
                                             <li>
-                                                <a href="#" className="dropdown__link">Free templates</a>
+                                                <a href="#" className="dropdown__link">LinkedIn</a>
                                             </li>
                                             <li>
-                                                <a href="#" className="dropdown__link">Premium templates</a>
+                                                <a href="#" className="dropdown__link">Facebook</a>
                                             </li>
                                         </ul>
                                     </div>
@@ -183,13 +188,13 @@ const Navbar = () => {
                         </li>
 
                         <li>
-                            <a href="#" className="nav__link">Pricing</a>
+                            <a href="#" className="nav__link">Events</a>
                         </li>
 
                         {/* <!--=============== DROPDOWN 3 ===============--> */}
                         <li className="dropdown__item">                        
                             <div className="nav__link dropdown__button">
-                                Company <i className="ri-arrow-down-s-line dropdown__arrow"></i>
+                                Faqs <i className="ri-arrow-down-s-line dropdown__arrow"></i>
                             </div>
 
                             <div className="dropdown__container">
