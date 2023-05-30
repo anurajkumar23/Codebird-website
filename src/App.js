7347279ff53125432c4e5b6186b0fc4c5dfffb71
Puskar-Roy <puskarroy600@gmail.com>
2023-05-30 14:30:48 +0530

    Try To Create A Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
-import './App.css';
+import './css/App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header/>
   
       </header>
     </div>
