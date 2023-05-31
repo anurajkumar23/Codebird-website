959c07cefc756b4e2b7021aa96ca82116580ef43
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-05-31 19:09:24 +0530

    responsive navbar currently working

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,88 +1,19 @@
-import React from 'react'
-import "./navbar.css"
-import Logo from "../images/logo.png"
+import React from 'react';
+import {useState}  from 'react';
+import "./navbar.css";
+import Logo from "../images/logo.png";
+import "./assets/scss/styles.scss";
 import { Icon } from '@iconify/react';
 
 
-
-/* eslint-disable no-restricted-globals */
-/*=============== SHOW MENU ===============*/
-// const showMenu = (toggleId, navId) =>{
-//     const toggle = document.getElementById(toggleId),
-//           nav = document.getElementById(navId)
- 
-//     toggle.addEventListener('click', () =>{
-//         // Agregar clase show-menu a nav menu
-//         nav.classList.toggle('show-menu')
-//         // Agregar show-icon para mostrar y ocultar el icono del menú
-//         toggle.classList.toggle('show-icon')
-//     })
-//  }
- 
-//  showMenu('nav-toggle','nav-menu')
- 
-//  /*=============== SHOW DROPDOWN MENU ===============*/
-//  const dropdownItems = document.querySelectorAll('.dropdown__item')
- 
-//  // 1. Selecionar cada dropdown item
-//  dropdownItems.forEach((item) =>{
-//      const dropdownButton = item.querySelector('.dropdown__button') 
- 
-//      // 2. Selecionar cada click del botón
-//      dropdownButton.addEventListener('click', () =>{
-//          // 7. Seleccionar la clase show-dropdown actual
-//          const showDropdown = document.querySelector('.show-dropdown')
-         
-//          // 5. Llamar a la funcion toggleItem
-//          toggleItem(item)
- 
-//          // 8. Remover la clase show-dropdown de otros items
-//          if(showDropdown && showDropdown!== item){
-//              toggleItem(showDropdown)
-//          }
-//      })
-//  })
- 
-//  // 3. Crear una función para mostrar el dropdown
-//  const toggleItem = (item) =>{
-//      // 3.1. Selecionar cada dropdown content
-//      const dropdownContainer = item.querySelector('.dropdown__container')
- 
-//      // 6. Si el mismo item contiene la clase show-dropdown, remover
-//      if(item.classList.contains('show-dropdown')){
-//          dropdownContainer.removeAttribute('style')
-//          item.classList.remove('show-dropdown')
-//      } else{
-//          // 4. Agregar el height maximo al dropdown content y agregar la clase show-dropdown
-//          dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px'
-//          item.classList.add('show-dropdown')
-//      }
-//  }
- 
-//  /*=============== DELETE DROPDOWN STYLES ===============*/
-//  const mediaQuery = matchMedia('(min-width: 1118px)'),
-//        dropdownContainer = document.querySelectorAll('.dropdown__container')
- 
-//  // Función para eliminar estilos desplegables en modo móvil cuando el navegador cambia de tamaño
-//  const removeStyle = () =>{
-//      // Validar si la media query llega a 1118px
-//      if(mediaQuery.matches){
-//          // Removemos el estilo de height de dropdown container
-//          dropdownContainer.forEach((e) =>{
-//              e.removeAttribute('style')
-//          })
- 
-//          // Removemos la clase show-dropdown de dropdown item
-//          dropdownItems.forEach((e) =>{
-//              e.classList.remove('show-dropdown')
-//          })
-//      }
-//  }
- 
-//  addEventListener('resize', removeStyle)
-
- 
 const Navbar = () => {
+    
+    const [isMenuVisible, setMenuVisible] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuVisible(!isMenuVisible);  
+      };
+
   return (
     <div>
       <header className="header">
@@ -96,15 +27,18 @@ const Navbar = () => {
                     </a>
                     </div>
                    </div>
-    
-                    <div className="nav__toggle" id="nav-toggle">
-                        <Icon icon="ri:menu-line nav__toggle-menu"/> 
-                        <Icon icon="ri:close-line nav__toggle-close"/> 
-                    </div>
+                   <button class="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+                        <i class="ri-menu-line nav__toggle-menu"></i>
+                        <i class="ri-close-line nav__toggle-close"></i>
+                     </button>
+                   {/* <div class="nav__toggle" id="nav-toggle">
+                        <i class="ri-menu-line nav__toggle-menu"></i>
+                        <i class="ri-close-line nav__toggle-close"></i>
+                    </div> */}
                 </div>
 
                 {/* <!--=============== NAV MENU ===============--> */}
-                <div className="nav__menu" id="nav-menu">
+                <div id="nav-menu" className={isMenuVisible ? 'nav__menu' : ''} >
                     <ul className="nav__list">
                         <li>
                             <a href="#" className="nav__link">Home</a>
