2f492d5b678c9b2ccdb704571548951af470e9e4
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-06-06 14:17:33 +0530

    stars

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Navbar from './Component/Navbar/Navbar';
+// import StarsCanvas from './Component/Star/Star';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <Navbar/>
+      {/* <StarsCanvas/> */}
       </header>
     </div>
   );
